45ab03b203b47ad8afa6be3334624fcc55a788ef
Liam Paull <paulll@iro.umontreal.ca>
2024-11-26 17:56:09 -0500

    updating torch and torchvision versions

--- assets/amd64/install.sh
+++ assets/amd64/install.sh
@@ -3,8 +3,8 @@
 set -ex
 
 # install PyTorch
-pip3 install torch==1.7.1
-pip3 install torchvision==0.8.1
+pip3 install torch==2.5.1
+pip3 install torchvision==0.20.1
 
 # clean
 pip3 uninstall -y dataclasses
